40fcad59adb758d3f5df3b295ad8f433a5ba0acd
enoch012 <zzan6238@naver.com>
2023-07-20 16:50:59 +0900

    0720 / react의 비동기 Axios - 영화 일간 순위 console.log 로 출력 - table 로 출력하는 예제

---
+++ src/App11.js
@@ -0,0 +1,18 @@
+import React from "react";
+import AxiosTest from "./study06/AxiosTest";
+import AxiosDailyBoxOffice from "./study06/AxiosDailyBoxOffice";
+
+
+function App11(props) {
+
+  return(
+    <div className={'container my-4'}>
+      {/*<AxiosTest />*/}
+
+      <AxiosDailyBoxOffice />
+    </div>
+  );
+}
+
+
+export default App11;
